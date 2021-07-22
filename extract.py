8f938c83b0a1da2cb20593ac862d8297f8004c1d
FHL-GITHUB <55864600+FHL-GITHUB@users.noreply.github.com>
2021-07-22 15:16:03 +0800

    Extract the bounding box from the received image

--- extract.py
+++ extract.py
@@ -3,50 +3,32 @@ import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
 
-
 def extractBBox(image):
-
-    #clear the images in the sub directory
+    # clear the images in the sub directory
     savedir = os.path.join(os.getcwd(), 'predict_result/result')
     if not os.path.exists(savedir):
         os.makedirs(savedir)
             
-    #t#est = os.listdir(savedir)
-    #for f in test:
-    #    if f.endswith(".jpg"):
-     #       os.remove(os.path.join(savedir, f))
-
-    #filelist = glob.glob(os.path.join(savedir, "*.jpg"))
-    #for f in filelist:
-    #  os.remove(f)
-
-
     boxes = []
     bb=[]
     transform = transforms.ToTensor()
 
     new_image = image.copy()
-    #new_image = new_image[100:,:]
 
     filelist = glob.glob(os.path.join(savedir, "*.jpg"))
     for f in filelist:
       os.remove(f)
     
-    #Adjust cropped image size
+    # adjust cropped image size
     #new_image = new_image[100:350, 50:590, :]
     #cv2.imshow('Image with bounding box', new_image)
     #cv2.waitKey(0) # Wait for keypress to continue
     #cv2.destroyAllWindows()
 
-
     gray = cv2.cvtColor(new_image, cv2.COLOR_RGB2GRAY) # convert to grayscale
     blurred = cv2.GaussianBlur(gray, (5,5), 0) # apply gaussian blur to blur the background
     thresh0 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 0)
     contours, _ = cv2.findContours(thresh0, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    
-    #with_contours = cv2.drawContours(new_image, contours, -1,(0,255,0),1)
-
     #print('Total number of contours detected: ' + str(len(contours)))
 
     count = 0
@@ -75,24 +57,15 @@ def extractBBox(image):
         predImg = img.copy()
         images.append(predImg)
 
-
         #img = cv2.resize(img,(32,32))
         img = cv2.resize(img,(224,224))
         #cv2.imshow('Gray image', img)  
-
 
         img = Image.fromarray(img)
         img = transform(img)
         img.requires_grad=False
         bb.append(img)
 
-        #raw_contour = cv2.drawContours(new_image, contours, i,(255,255,255), 1)
-        #rects = cv2.rectangle(predImg,(x,y), (x+w,y+h), (0,0,255), 2)
-        #cv2.putText(predImg, 'BB_'+str(count), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-        #print(rects)
-        #cv2.imshow('Image with bounding box', rects)
-        #cv2.waitKey(0) # Wait for keypress to continue
-        #cv2.destroyAllWindows()
         boxes.append(np.array(rect))
         count+=1
     
