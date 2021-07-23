e50cfc4d14b9ab79be482ca040f8eed03d9dc065
FHL-GITHUB <55864600+FHL-GITHUB@users.noreply.github.com>
2021-07-23 11:23:55 +0800

    Create resave_model.py

---
+++ Tools/resave_model.py
@@ -0,0 +1,20 @@
+import torch
+from efficientnet_pytorch import EfficientNet
+from torchvision.transforms import Compose, ToTensor, Resize, Grayscale
+import torch.nn as nn
+
+def do_nothing():
+  pass
+
+if __name__ == '__main__':
+    net = EfficientNet.from_pretrained('efficientnet-b0')
+    net._fc = nn.Sequential(nn.Linear(net._fc.in_features, 512), 
+                                           nn.ReLU(),  
+                                           nn.Dropout(0.25),
+                                           nn.Linear(512, 128), 
+                                           nn.ReLU(),  
+                                           nn.Dropout(0.50), 
+                                           nn.Linear(128,16))
+    net.load_state_dict(torch.load(r'newCNNmodel.pt', map_location=torch.device('cpu')))
+    net.eval()
+    torch.save(net, "./model_dic_color.pt")
